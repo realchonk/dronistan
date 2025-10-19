ac82fc636c567590690efc7e0acddf54293ed256
Benjamin Stürz <benni@stuerz.xyz>
2025-10-19 20:18:41 +0000

    use other tech

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,52 @@
-use openh264::decoder::Decoder;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
-use sdl2::surface::Surface;
-use sdl2::{event::Event, render::Texture};
-use tokio::sync::Mutex;
-use std::sync::Arc;
-use std::time::Duration;
-use tello_edu::{Tello, TelloOptions, VIDEO_HEIGHT, VIDEO_WIDTH};
+use std::{future::ready, sync::Arc};
 
-#[tokio::main]
-async fn main() -> Result<(), String> {
+use iced::{
+    Element, Subscription, Task,
+    futures::{
+        Stream, StreamExt,
+    },
+    widget::{button, column, Image},
+    advanced::image::Handle as ImageHandle,
+};
+use openh264::{decoder::{DecodedYUV, Decoder}, formats::YUVSource};
+use option_lock::OptionLock;
+use tello_edu::{Tello, TelloOptions, TelloVideoFrame, tello::Connected};
+use tokio::sync::mpsc::UnboundedReceiver;
+
+#[derive(Default)]
+enum Screen {
+    #[default]
+    Start,
+
+    Connecting,
+
+    Connected {
+        drone: Arc<Drone>,
+        image: ImageHandle,
+    },
+}
+
+#[derive(Default)]
+struct State {
+    screen: Screen,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Start,
+    Connected(Arc<Drone>),
+    Frame(ImageHandle),
+}
+
+#[derive(Debug)]
+struct Drone {
+    drone: Tello<Connected>,
+    video: OptionLock<UnboundedReceiver<TelloVideoFrame>>,
+}
+
+async fn connect_drone() -> Arc<Drone> {
     let mut options = TelloOptions::default();
-
-    let mut video = options.with_video();
-
+    let video = OptionLock::new(options.with_video());
     let drone = Tello::new()
         .wait_for_wifi()
         .await
@@ -22,93 +55,75 @@ async fn main() -> Result<(), String> {
         .await
         .unwrap();
 
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-    let window = video_subsystem
-        .window("TELLO drone", VIDEO_WIDTH, VIDEO_HEIGHT)
-        .build()
-        .unwrap();
-    let mut canvas = window.into_canvas().build().unwrap();
+    Arc::new(Drone { drone, video })
+}
 
+fn map_frame(frame: DecodedYUV<'_>) -> Message {
+    let (width, height) = frame.dimensions_uv();
+    let mut pixels = vec![0u8; frame.rgba8_len()];
+    frame.write_rgba8(&mut pixels);
+    let handle = ImageHandle::from_rgba(width as u32, height as u32, pixels);
+    Message::Frame(handle)
+}
 
-    drone.send("downvision 1").await.unwrap();
-    drone.send("setbitrate 1").await.unwrap();
-
-    drone.start_video().await.unwrap();
-
-    println!("Startuyem!");
-
-    let inspect = |r| match r {
-        Ok(()) => {}
-        Err(e) => println!("Received Error: {e}"),
-    };
-
-    let rendered = Arc::new(Mutex::new(None));
-    let r2 = Arc::clone(&rendered);
-    let mut surface = Surface::new(VIDEO_WIDTH, VIDEO_HEIGHT, PixelFormatEnum::RGB24).unwrap();
-
-    let decode_task = async move {
-        let mut decoder = Decoder::new().unwrap();
-        while let Some(packet) = video.recv().await {
+fn decode_video(drone: Arc<Drone>) -> impl Stream<Item = Message> {
+    let video = drone.video.try_take().unwrap();
+    let mut decoder = Decoder::new().unwrap();
+    tokio_stream::wrappers::UnboundedReceiverStream::new(video)
+        .map(move |packet| {
             match decoder.decode(&packet.data) {
-                Ok(Some(frame)) => {
-                    r2.lock().await.replace(frame);
+                Ok(Some(frame)) => Some(map_frame(frame)),
+                Ok(None) => None,
+                Err(e) => {
+                    eprintln!("Failed to decode frame: {e}");
+                    None
                 },
-                Ok(None) => println!("No frame"),
-                Err(e) => println!("Oh no, got decodi error: {e}"),
             }
+        })
+        .filter_map(ready)
+}
+
+fn update(state: &mut State, msg: Message) -> Task<Message> {
+    match msg {
+        Message::Start => {
+            println!("Startuyem!");
+            state.screen = Screen::Connecting;
+            Task::perform(connect_drone(), Message::Connected)
         }
-    };
-
-    tokio::spawn(decode_task);
-
-    let mut event_pump = sdl_context.event_pump()?;
-    'running: loop {
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } => break 'running,
-                Event::KeyDown {
-                    keycode: Some(kc), ..
-                } => match kc {
-                    Keycode::Q => break 'running,
-                    Keycode::K => inspect(drone.take_off().await),
-                    Keycode::L => inspect(drone.land().await),
-                    Keycode::W => inspect(drone.move_forward(20).await),
-                    Keycode::S => inspect(drone.move_back(20).await),
-                    Keycode::A => inspect(drone.move_left(20).await),
-                    Keycode::D => inspect(drone.move_right(20).await),
-                    Keycode::Period => inspect(drone.move_down(20).await),
-                    Keycode::Comma => inspect(drone.move_up(20).await),
-                    _ => {}
-                },
-                Event::KeyUp {
-                    keycode: Some(kc), ..
-                } => match kc {
-                    Keycode::W
-                    | Keycode::S
-                    | Keycode::A
-                    | Keycode::D
-                    | Keycode::Period
-                    | Keycode::Comma => drone.stop().await.unwrap(),
-                    _ => {}
-                },
-                _ => {}
+        Message::Connected(drone) => {
+            let copy = Arc::clone(&drone);
+            state.screen = Screen::Connected {
+                drone,
+                image: ImageHandle::from_path("empty.png"),
+            };
+            Task::stream(decode_video(copy))
+        }
+        Message::Frame(frame) => {
+            match &mut state.screen {
+                Screen::Connected { image, .. } => *image = frame,
+                _ => {},
             }
-        }
-
-
-        if let Some(mut x) = rendered.try_lock().ok() {
-            if let Some(x) = x.take() {
-                surface.with_lock_mut(|pixels| {
-                    x.write_rgb8(pixels);
-                });
-            }
-        }
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
+            Task::none()
+        },
     }
+}
 
-    Ok(())
+fn view(state: &State) -> Element<'_, Message> {
+    match &state.screen {
+        Screen::Start => {
+            column!["Hello Drone Man!", button("Start").on_press(Message::Start),].into()
+        }
+        Screen::Connecting => "Connecting to drone...".into(),
+        Screen::Connected { drone, image } => Image::new(image.clone()).into(),
+    }
+}
+
+fn subscription(state: &State) -> Subscription<Message> {
+    todo!()
+}
+
+fn main() -> iced::Result {
+    iced::application("Dronistan", update, view)
+        //.subscription(subscription)
+        .run()
 }
